9607b0bfd394935a1fe90d62c8201a278077166a
Antony Blem <95442431+SkyKingPX@users.noreply.github.com>
2023-08-02 16:23:46 +0200

    Save project

--- images.g.ts
+++ images.g.ts
@@ -3,25 +3,6 @@ namespace myImages {
 
     helpers._registerFactory("image", function(name: string) {
         switch(helpers.stringTrim(name)) {
-            case "image1":
-            case "bock":return img`
-7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
-7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
-7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
-7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
-7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
-7 7 7 7 7 7 e 7 7 7 7 7 7 7 e 7 
-7 e e 7 7 7 e e 7 e e 7 7 e e 7 
-e e e e e 7 e e e e e 7 e e e e 
-e e e e e e e e e e e e e e e e 
-e e e e e e e e e e e e e e e e 
-e e e e e e e e e e e e e e e e 
-e e e e e e e e e e e e e e e e 
-e e e e e e e e e e e e e e e e 
-e e e e e e e e e e e e e e e e 
-e e e e e e e e e e e e e e e e 
-e e e e e e e e e e e e e e e e 
-`;
             case "image2":
             case "Kristal":return img`
 . . . . . . . 9 9 . . . . . . . 
@@ -40,6 +21,45 @@ e e e e e e e e e e e e e e e e
 . . . . . 9 9 9 9 9 9 . . . . . 
 . . . . . . 9 9 9 9 . . . . . . 
 . . . . . . 9 9 9 9 . . . . . . 
+`;
+            case "8oxU_TiwSDGlslGE5v{u":
+            case "PlayerHinten":return img`
+. . . . . . . c c c . . . . . . 
+. . . . . . c b 9 c . . . . . . 
+. . . . c c c 1 1 c c c . . . . 
+. . c c b c 1 1 1 1 c c c c . . 
+. c b b 1 b 1 1 1 1 b 1 b b c . 
+. c b 1 1 b b 1 1 b b 1 1 b c . 
+. . f 1 1 1 b b b b 1 1 1 c . . 
+. . f f 1 1 1 1 1 1 1 1 f f . . 
+. . f f f f f f f f f f f f . . 
+. . f f f f f f f f f f f f . . 
+. . . f f f f f f f f f f . . . 
+. . . e e f f f f f f e e . . . 
+. . e b c b 1 b b 1 b f b e . . 
+. . e e f 9 1 1 1 1 9 f e e . . 
+. . . . c b 1 1 1 1 b c . . . . 
+. . . . . f f f f f f . . . . . 
+`;
+            case ":iH:/[40S~=":
+            case "Maus":return img`
+. . . . . . . . . . . . . . . . . 
+. . . 3 3 3 3 3 3 3 . . . . . . . 
+. . 3 3 3 3 3 3 3 3 3 . . . . . . 
+. 3 3 3 1 1 1 1 1 3 3 3 . . . . . 
+. 3 3 1 1 1 1 1 1 1 3 3 . . . . . 
+. 3 3 1 1 1 1 1 1 1 3 3 . . . . . 
+. 3 3 1 1 1 1 1 1 1 3 3 . . . . . 
+. 3 3 1 1 1 1 1 1 1 3 3 . . . . . 
+. 3 3 d 1 1 1 1 1 1 3 3 . . . . . 
+. 3 3 3 d d d d d 3 3 3 . . . . . 
+. . 3 3 3 3 3 3 3 3 3 3 3 . . . . 
+. . . 3 3 3 3 3 3 3 3 e 3 3 . . . 
+. . . . . . . . . . 3 3 e 3 3 . . 
+. . . . . . . . . . . 3 3 e 3 . . 
+. . . . . . . . . . . . 3 3 3 . . 
+. . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . 
 `;
         }
         return null;
